4bd58adb73e8fcf5f378d1d4a73c14f4578090f6
framefess <59738687+framefess@users.noreply.github.com>
2021-05-15 11:15:06 +0700

    trigger server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express")
-
+const fetch = require('node-fetch');
 const server = express()
 
 server.all("/",(req,res) =>{
@@ -11,5 +11,32 @@ function keepalive() {
     console.log("server is ready");
     })
 }
+
+
+
+// globals
+const interval = 25*60*1000; // interval in milliseconds - {25mins x 60s x 1000}ms
+const url = "https://awtlm-wax.herokuapp.com/";
+
+(function wake() {
+
+  try {
+
+    const handler = setInterval(() => {
+
+      fetch(url)
+        .then(res => console.log(`response-ok: ${res.ok}, status: ${res.status}`))
+        .catch(err => console.error(`Error occured: ${err}`));
+
+    }, interval);
+
+  } catch(err) {
+      console.error('Error occured: retrying...');
+      clearInterval(handler);
+      return setTimeout(() => wake(), 10000);
+  };
+
+})();
+
 
 module.exports = keepalive
